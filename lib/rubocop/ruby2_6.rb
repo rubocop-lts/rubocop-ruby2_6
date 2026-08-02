cbb47350772cb5130e064b5aa56195755eb2dc25
Peter H. Boling <peter.boling@gmail.com>
2026-08-02 15:52:52 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/rubocop/ruby2_6.rb
+++ lib/rubocop/ruby2_6.rb
@@ -2,9 +2,9 @@
 
 # external libs
 require "version_gem"
+require_relative "ruby2_6/version"
 
 # this gem
-require_relative "ruby2_6/version"
 # simplecov:disable
 require_relative "ruby2_6/railtie" if defined?(Rails::Railtie)
 # simplecov:enable
